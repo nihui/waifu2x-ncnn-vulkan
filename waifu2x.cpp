b793f3a8fed57a922faac3ddfad24b481977a48b
nihui <shuizhuyuanluo@126.com>
2019-04-06 23:37:18 +0800

    use wic on windows, fix odd size issue, fix #3

--- waifu2x.cpp
+++ waifu2x.cpp
@@ -4,7 +4,11 @@
 #include <algorithm>
 #include <vector>
 
-// image decoder and encoder
+#if WIN32
+// image decoder and encoder with wic
+#include "wic_image.h"
+#else // WIN32
+// image decoder and encoder with stb
 #define STB_IMAGE_IMPLEMENTATION
 #define STBI_NO_PSD
 #define STBI_NO_TGA
@@ -14,6 +18,7 @@
 #include "stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
+#endif // WIN32
 
 // ncnn
 #include "layer_type.h"
@@ -61,6 +66,10 @@ int main(int argc, char** argv)
         sprintf(modelpath, "models-cunet/noise%d_scale2.0x_model.bin", noise);
     }
 
+#if WIN32
+    CoInitialize(0);
+#endif
+ 
     ncnn::create_gpu_instance();
 
     ncnn::VulkanDevice* vkdev = new ncnn::VulkanDevice;
@@ -76,6 +85,7 @@ int main(int argc, char** argv)
 
         // preprocess and postprocess operator
         ncnn::Layer* pre_padding = 0;
+        ncnn::Layer* post_crop = 0;
         ncnn::Layer* normalize = 0;
         ncnn::Layer* denormalize = 0;
         ncnn::Layer* cast_float32_to_float16 = 0;
@@ -96,10 +106,10 @@ int main(int argc, char** argv)
                 pre_padding->vkdev = vkdev;
 
                 ncnn::ParamDict pd;
-                pd.set(0, prepadding);
-                pd.set(1, prepadding);
-                pd.set(2, prepadding);
-                pd.set(3, prepadding);
+                pd.set(0, -233);
+                pd.set(1, -233);
+                pd.set(2, -233);
+                pd.set(3, -233);
                 pd.set(4, ncnn::BORDER_REPLICATE);
                 pd.set(5, 0.f);
                 pd.use_vulkan_compute = 1;
@@ -107,6 +117,21 @@ int main(int argc, char** argv)
                 pre_padding->load_param(pd);
 
                 pre_padding->create_pipeline();
+            }
+
+            {
+                post_crop = ncnn::create_layer(ncnn::LayerType::Crop);
+                post_crop->vkdev = vkdev;
+
+                ncnn::ParamDict pd;
+                pd.set(0, 0);
+                pd.set(1, 0);
+                pd.set(2, 0);
+                pd.use_vulkan_compute = 1;
+
+                post_crop->load_param(pd);
+
+                post_crop->create_pipeline();
             }
 
             {
@@ -217,6 +242,19 @@ int main(int argc, char** argv)
 
         // main routine
         {
+#if WIN32
+            int w, h, c;
+            unsigned char* bgrdata = wic_decode_image(imagepath, &w, &h, &c);
+            if (!bgrdata)
+            {
+                fprintf(stderr, "decode image %s failed\n", imagepath);
+                return -1;
+            }
+
+            ncnn::Mat in = ncnn::Mat::from_pixels(bgrdata, ncnn::Mat::PIXEL_BGR2RGB, w, h);
+
+            free(bgrdata);
+#else // WIN32
             int w, h, c;
             unsigned char* rgbdata = stbi_load(imagepath, &w, &h, &c, 3);
             if (!rgbdata)
@@ -228,6 +266,7 @@ int main(int argc, char** argv)
             ncnn::Mat in = ncnn::Mat::from_pixels(rgbdata, ncnn::Mat::PIXEL_RGB, w, h);
 
             stbi_image_free(rgbdata);
+#endif // WIN32
 
             ncnn::VkCompute cmd(vkdev);
 
@@ -250,18 +289,43 @@ int main(int argc, char** argv)
                 in_gpu = in_gpu_fp16;
             }
 
-            // prepadding
-            {
-                ncnn::VkMat in_gpu_padded;
-                pre_padding->forward(in_gpu, in_gpu_padded, cmd, opt);
-                in_gpu = in_gpu_padded;
-            }
-
             // normalize
             {
                 ncnn::VkMat in_gpu_normed;
                 normalize->forward(in_gpu, in_gpu_normed, cmd, opt);
                 in_gpu = in_gpu_normed;
+            }
+
+            // prepadding
+            int prepadding_bottom = prepadding;
+            int prepadding_right = prepadding;
+            if (scale == 1)
+            {
+                prepadding_bottom += (h + 3) / 4 * 4 - h;
+                prepadding_right += (w + 3) / 4 * 4 - w;
+            }
+            if (scale == 2)
+            {
+                prepadding_bottom += (h + 1) / 2 * 2 - h;
+                prepadding_right += (w + 1) / 2 * 2 - w;
+            }
+            {
+                ncnn::VkMat pre_padding_params(4, (size_t)4u, 1, opt.blob_vkallocator, opt.staging_vkallocator);
+                pre_padding_params.prepare_staging_buffer();
+                int* padding_param = pre_padding_params.mapped();
+
+                padding_param[0] = prepadding;
+                padding_param[1] = prepadding_bottom;
+                padding_param[2] = prepadding;
+                padding_param[3] = prepadding_right;
+
+                std::vector<ncnn::VkMat> pre_padding_inputs(2);
+                pre_padding_inputs[0] = in_gpu;
+                pre_padding_inputs[1] = pre_padding_params;
+
+                std::vector<ncnn::VkMat> pre_padding_outputs(1);
+                pre_padding->forward(pre_padding_inputs, pre_padding_outputs, cmd, opt);
+                in_gpu = pre_padding_outputs[0];
             }
 
             ncnn::VkMat out_gpu;
@@ -286,9 +350,9 @@ int main(int argc, char** argv)
                     {
                         // crop tile
                         int tile_x0 = xi * TILE_SIZE_X;
-                        int tile_x1 = std::min((xi+1) * TILE_SIZE_X, w) + prepadding + prepadding;
+                        int tile_x1 = std::min((xi+1) * TILE_SIZE_X, w) + prepadding + prepadding_right;
                         int tile_y0 = yi * TILE_SIZE_Y;
-                        int tile_y1 = std::min((yi+1) * TILE_SIZE_Y, h) + prepadding + prepadding;
+                        int tile_y1 = std::min((yi+1) * TILE_SIZE_Y, h) + prepadding + prepadding_bottom;
 
                         crop_param[0] = tile_x0;
                         crop_param[1] = tile_y0;
@@ -327,6 +391,22 @@ int main(int argc, char** argv)
                 out_gpu = merge_tile_y_outputs[0];
             }
 
+            // postcrop
+            {
+                ncnn::VkMat crop_reference_blob;
+                crop_reference_blob.dims = 2;
+                crop_reference_blob.w = w * scale;
+                crop_reference_blob.h = h * scale;
+
+                std::vector<ncnn::VkMat> post_crop_inputs(2);
+                post_crop_inputs[0] = out_gpu;
+                post_crop_inputs[1] = crop_reference_blob;
+
+                std::vector<ncnn::VkMat> post_crop_outputs(1);
+                post_crop->forward(post_crop_inputs, post_crop_outputs, cmd, opt);
+                out_gpu = post_crop_outputs[0];
+            }
+
             // denormalize
             {
                 ncnn::VkMat out_gpu_denormed;
@@ -356,10 +436,17 @@ int main(int argc, char** argv)
             out.create_like(out_gpu, opt.blob_allocator);
             out_gpu.download(out);
 
-            ncnn::Mat outrgb(out.w, out.h, 1, 3u, 3);
+#if WIN32
+            ncnn::Mat outbgr(out.w, out.h, (size_t)3u, 3);
+            out.to_pixels((unsigned char*)outbgr.data, ncnn::Mat::PIXEL_RGB2BGR);
+
+            int ret = wic_encode_image(outputpngpath, outbgr.w, outbgr.h, 3, outbgr.data);
+#else
+            ncnn::Mat outrgb(out.w, out.h, (size_t)3u, 3);
             out.to_pixels((unsigned char*)outrgb.data, ncnn::Mat::PIXEL_RGB);
 
             int ret = stbi_write_png(outputpngpath, outrgb.w, outrgb.h, 3, outrgb.data, 0);
+#endif
             if (ret == 0)
             {
                 fprintf(stderr, "encode image %s failed\n", outputpngpath);
@@ -389,6 +476,9 @@ int main(int argc, char** argv)
 
             pre_padding->destroy_pipeline();
             delete pre_padding;
+
+            post_crop->destroy_pipeline();
+            delete post_crop;
 
             normalize->destroy_pipeline();
             delete normalize;
